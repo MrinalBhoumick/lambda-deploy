5c877c1d6cc4db6c9bb907d66a95fbf589b0d42d
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-06-24 00:51:09 +0530

    updated Code

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 // Lambda function handler
 exports.handler = async (event) => {
   try {
-    // Get current date and time
-    const currentTime = new Date();
-    const hour = currentTime.getUTCHours();
+    // Determine which route is being accessed
+    const route = event.requestContext.http.path;
 
-    // Determine greeting based on time of day
-    let greeting;
-    if (hour < 12) {
-      greeting = 'Good morning';
-    } else if (hour < 18) {
-      greeting = 'Good afternoon';
+    // Handle different routes
+    if (route === '/greeting') {
+      // Get current date and time
+      const currentTime = new Date();
+      const hour = currentTime.getUTCHours();
+
+      // Determine greeting based on time of day
+      let greeting;
+      if (hour < 12) {
+        greeting = 'Good morning';
+      } else if (hour < 18) {
+        greeting = 'Good afternoon';
+      } else {
+        greeting = 'Good evening';
+      }
+
+      // Construct greeting message
+      const message = `${greeting}, User!`;
+
+      // Return successful response
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message }),
+      };
     } else {
-      greeting = 'Good evening';
+      // Handle unknown routes
+      throw new Error('Unsupported route');
     }
-
-    // Construct greeting message
-    const message = `${greeting}, User!`;
-
-    // Return successful response
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message }),
-    };
   } catch (error) {
     // Return error response
     return {
