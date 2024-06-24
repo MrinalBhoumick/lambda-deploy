cd69496d884136ecc1508fc9d8f5bdb87035cf53
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-06-24 14:33:00 +0530

    Added some quotes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const customQuotes = [
   "Strive not to be a success, but rather to be of value. - Albert Einstein",
   "The best way to predict the future is to create it. - Abraham Lincoln",
   "Believe in Yourself. - Mrinal Bhoumick",
-  "Just have prayings for me have one crucial exam. - Mrinal Bhoumick"
+  "Just have prayings for me have one crucial exam. - Mrinal Bhoumick",
+  "Wherever Life takes you bloom with grace. - Mrinal Bhoumick"
 ];
 
 const motivationalQuotes = [
